ccd9d532c4e3b763107b1c5177a06d91433bc70f
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-20 16:06:53 +0100

    Documener started

---
+++ docs/make.jl
@@ -0,0 +1,9 @@
+using Documenter, SOM
+ 
+makedocs(modules=[SOM],
+         doctest=true)
+ 
+deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
+           repo   = "github.com/andreasdominik/SOM.jl.git",
+           julia  = "0.6.2",
+           osname = "linux")
