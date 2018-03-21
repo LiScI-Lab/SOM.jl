1c1c3e34a5b39e4d82e0dc40e079060d14e37ff9
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-21 11:18:16 +0100

    set up github-pages according to https://juliadocs.github.io/Documenter.jl/stable/man/hosting/

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,5 @@ makedocs(modules=[SOM],
 
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
            repo   = "github.com/andreasdominik/SOM.jl.git",
-           target = build,
            julia  = "0.6.2",
            osname = "linux")
