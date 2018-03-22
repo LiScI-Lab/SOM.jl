68d27360e2ae4e7fea88e0c998a3b69a2ec0f569
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-22 14:19:31 +0100

    bugfix in testVisual

--- test/testFuns.jl
+++ test/testFuns.jl
@@ -24,6 +24,23 @@ function testVisual(train, topol, toroidal)
     tr  = train[1:50,:]
     val = train[51:100,:]
 
+    som = initSOM(tr, xdim, ydim, norm = :zscore,
+    topol = topol, toroidal = toroidal)
+
+    vis = mapToSOM(som, tr)
+
+    ntr    = nrow(tr)
+    npopul = nrow(vis)
+
+    return ntr == npopul
+end
+
+
+function testFreqs(train, topol, toroidal)
+
+    xdim = 8
+    ydim = 10
+
     som = initSOM(train, xdim, ydim, norm = :zscore,
     topol = topol, toroidal = toroidal)
 
