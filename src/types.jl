bf2acb112368418e890c877a8fcaa976e08e6d26
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-22 15:59:23 +0100

    docu of type Som reviewed

--- src/types.jl
+++ src/types.jl
@@ -3,25 +3,25 @@
 """
     struct Som
 
-Stored data of a trained SOM.
+Structure to hold all data of a trained SOM.
 
 # Fields:
-- `codes`: 2D-array of codebook vectors. One vector per row
-- `colNames`: names of the attribute with which the SOM is trained
-- `normParams`: DataFrame with normalisation parameters for each column
+- `codes::Array{Float64,2}`: 2D-array of codebook vectors. One vector per row
+- `colNames::Array{String,1}`: names of the attribute with which the SOM is trained
+- `normParams::DataFrame`: normalisation parameters for each column
                 of training data. Column headers corresponds with
                 colNames.
-- `norm`: normalisation of training data; one of `:none, :minmax, :zscore`
-- `xdim`: number of neurons in x-direction
-- `ydim`: number of neurons in y-direction
-- `nCodes`: total number of neurons
-- `grid`: 2D-array of coordinates of neurons on the map
+- `norm::Symbol`: normalisation type; one of `:none, :minmax, :zscore`
+- `xdim::Int`: number of neurons in x-direction
+- `ydim::Int`: number of neurons in y-direction
+- `nCodes::Int`: total number of neurons
+- `grid::Array{Float64,2}`: 2D-array of coordinates of neurons on the map
           (2 columns (x,y)] for rectangular and hexagonal maps
            3 columns (x,y,z) for spherical maps)
-- `indices`: 2D-array of indices of the neurons
-- `topol`: topology of the SOM; one of `:rectangular, :hexagonal, :spherical`
-- `toroidal`: if `true`, the SOM is toroidal (no edges)
-- `population`: 1D-array of numbers of training samples mapped to
+- `indices::DataFrame`: X-, Y-indices of the neurons
+- `topol::Symbol`: topology of the SOM; one of `:rectangular, :hexagonal, :spherical`
+- `toroidal::Bool`: if `true`, the SOM is toroidal (has no edges)
+- `population::Array{Int,1}`: 1D-array of numbers of training samples mapped to
                 each neuron.
 """
 struct Som
