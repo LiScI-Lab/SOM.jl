4de31566cea429f6e70e2d80ff70927a8b8f3489
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-24 14:40:51 +0100

    try env via run(bash)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,8 @@
 #
 # ENV["LD_LIBRARY_PATH"] = "\$HOME/.julia/v0.6/Conda/deps/usr/lib:\$LD_LIBRARY_PATH"
 # ENV["LD_LIBRARY_PATH"] = Pkg.dir() * "/Conda/deps/usr/lib"
-println("Begin testing: LD_LIBRARY_PATH: $(ENV["LD_LIBRARY_PATH"])")
+run(`bash -c 'export LD_LIBRARY_PATH=\$HOME/.julia/v0.6/Conda/deps/usr/lib:\$LD_LIBRARY_PATH'`)
+# println("Begin testing: LD_LIBRARY_PATH: $(ENV["LD_LIBRARY_PATH"])")
 
 using SOM
 using Base.Test
@@ -38,10 +39,10 @@ include("testFuns.jl")
 
 # test plots:
 #
-ld_path = ENV["LD_LIBRARY_PATH"]
-println("Plots testing: LD_LIBRARY_PATH: $ld_path")
-
-if contains(ld_path, "Conda")
+# ld_path = ENV["LD_LIBRARY_PATH"]
+# println("Plots testing: LD_LIBRARY_PATH: $ld_path")
+#
+# if contains(ld_path, "Conda")
     @test testDensityPlot(train, :rectangular)
     @test testDensityPlot(train, :hexagonal)
     @test testDensityPlot(train, :spherical)
@@ -51,4 +52,4 @@ if contains(ld_path, "Conda")
     @test testClassesPlot(train, iris, :rectangular)
     @test testClassesPlot(train, iris, :hexagonal)
     @test testClassesPlot(train, iris, :spherical)
-end
+# end
