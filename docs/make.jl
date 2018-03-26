3f6291a5e65f60ff06c7e9db83eec70d2809290f
Andreas Dominik <andreas.dominik@mni.thm.de>
2018-03-26 11:47:55 +0200

    Documenter configured to generate html (w/o makedocs)

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,41 @@
+# using Documenter, SOM
+#
+# makedocs(modules=[SOM],
+#          doctest=true)
+#
+# deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
+#            repo   = "github.com/andreasdominik/SOM.jl.git",
+#            julia  = "0.6",
+#            osname = "linux")
+#
 using Documenter, SOM
 
 makedocs(modules=[SOM],
-         doctest=true)
+         clean = false,
+         format = :html,
+         assets = ["assets/favicon.ico"],
+         sitename = "SOM.jl",
+         authors = "Andreas Dominik",
+         pages = [
+                  "Introduction" => "index.md",
+                  "Tutorials" => Any[
+                            "Quick Start" => "tutorials/firstTutorial.md"
+                                     ],
+                   "API" => Any[
+                            "Types" => "api/types.md",
+                            "Training" => "api/soms.md",
+                            "Visualisation" => "api/visualisations.md"
+                               ],
+                    "License" => "LICENSE.md"
+                  ],
+                  # Use clean URLs, unless built as a "local" build
+          html_prettyurls = !("local" in ARGS),
+          html_canonical = "https://andreasdominik.github.io/SOM.jl/stable/",
+         )
 
-deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-           repo   = "github.com/andreasdominik/SOM.jl.git",
+deploydocs(repo   = "github.com/andreasdominik/SOM.jl.git",
            julia  = "0.6",
-           osname = "linux")
+           osname = "linux",
+           target = "build",
+           deps = nothing,
+           make = nothing)
